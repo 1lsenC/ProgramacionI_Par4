554798523e855424490dff1e9f93c178b7ba5e56
IlsenChivas <ilsen.chivas@ucb.edu.bo>
2026-08-19 01:16:52 -0400

    Ejercicios 2-6

---
+++ PRACTICA_00/Ejercicio_00_05.cpp
@@ -0,0 +1,22 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Ilsen Chivas Machaca
+// Carrera del estudiante: Ingenieria Mecatronica
+// Fecha creación: 19/08/2026
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,aux;
+
+    cout<<"Digite el valor de x: "; cin>>x;
+    cout<<"Digite el valor de y: "; cin>>y;
+
+    aux = x;
+    x = y;
+    y = aux;
+
+    cout<<"\nEl nuevo valor de x es: "<<x<<endl;
+    cout<<"El nuevo valor de y es: "<<y<<endl;
+    
+    return 0;
+}
